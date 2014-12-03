926172536be612083a6857c1df158737140e2c52
Jonathan van Wunnik <jonathan@artlantis.nl>
2014-12-03 15:08:17 +0100

    Update tags' usage description/comments.

--- site/tags/quote.php
+++ site/tags/quote.php
@@ -2,13 +2,18 @@
 /**
  * Quote
  * ----
- * Adds an inline quote tag.
+ * Inline quotation with language locale.
  *
  * What it does:
- * Wraps the text in <q> tag, with appropriate classes and language attribute
+ * Wraps  passed text string in <q> tag, with class `Quote`
+ * and passed or oterhwise default language attribute.
  *
  * Usage:
+ * (quote: This is an English inline quote)
  * (quote: This is an English inline quote lang: en)
+ *
+ * Example:
+ * http://altair.studiodumbar.com/base#paragraph
  */
 
 kirbytext::$tags['quote'] = array(
@@ -18,9 +23,15 @@ kirbytext::$tags['quote'] = array(
 	'html' => function($tag) {
 
 		$text = $tag->attr('quote');
-		$lang = $tag->attr('lang', c::get('lang.default', 'en'));
 
-		return '<q class="Quote" lang="' . $lang . '">' . $text . '</q>';
+		if($tag->attr('lang')) {
+			$language_locale = $tag->attr('lang');
+		}
+		else {
+			$language_locale = 'en'; // Fallback language locale if no is passed, e.g. 'en, nl_NL', 'de_DE', ect.
+		}
+
+		return '<q class="Quote" lang="' . $language_locale . '">' . $text . '</q>';
 
 	}
 );
