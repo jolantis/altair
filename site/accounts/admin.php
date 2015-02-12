e672bc9f09c36e40786dc845af4887c2dcb3ed67
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-02-12 17:12:03 +0100

    Add default panel admin account.

---
+++ site/accounts/admin.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: admin
+email: webadmin@studiodumbar.com
+password: >
+  $2a$10$9kv5q1im1eR6KZ6loyz1c.QJ9lFvMyfRfOv08uktRvvRzk6oPfvkK
+language: en
+role: admin
+firstname:
+lastname:
