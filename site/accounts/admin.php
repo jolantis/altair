2f2252dec2f7826148816ff55ff4b8cfe8ed10a7
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-02-16 10:35:09 +0100

    Update default (admin) user.

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -3,8 +3,7 @@
 username: admin
 email: webadmin@studiodumbar.com
 password: >
-  $2a$10$9kv5q1im1eR6KZ6loyz1c.QJ9lFvMyfRfOv08uktRvvRzk6oPfvkK
+  $2a$10$emo70u0FFvO3rkoxUlQUZOm9fB5yJOLHu4dC.xar9G4KpRd5cnjDS
 language: en
 role: admin
-firstname:
-lastname:
+token: beaf94db94d8653cd5cc0486435a5f83a9b9cfb8
