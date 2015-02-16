e60f44ca237927764469c3648a2c826652391d04
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-02-16 11:30:21 +0100

    Ignore (Kirby) panel account and assets files.
    Do not track panel accounts.

--- site/accounts/admin.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('KIRBY')) exit ?>
-
-username: admin
-email: webadmin@studiodumbar.com
-password: >
-  $2a$10$emo70u0FFvO3rkoxUlQUZOm9fB5yJOLHu4dC.xar9G4KpRd5cnjDS
-language: en
-role: admin
-token: beaf94db94d8653cd5cc0486435a5f83a9b9cfb8
