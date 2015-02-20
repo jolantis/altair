b2819bc7908881a9f54917b8e50aba966d05baf0
Marijn Tijhuis <marijn@studiodumbar.com>
2015-02-20 11:53:30 +0100

    Add resrc fix also to mobile scripts.

--- assets/javascript/mobile.scripts.js
+++ assets/javascript/mobile.scripts.js
@@ -7,6 +7,14 @@
 // Executed on DOM ready
 domready(function () {
 
+	// Initialize resrc.run() function
+	if(typeof custom_resrc === 'undefined') {
+		resrc.run();
+	}
+	else {
+		resrc.configure(custom_resrc).run();
+	}
+
 	alerts.init(push_message);                // Init alerts
 	expand.init();                            // Init expand / collapse
 	navMain.init();                           // Init main navigation
