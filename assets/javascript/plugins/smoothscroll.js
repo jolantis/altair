033917030b2ebb7c97462d8397bc4bfb30c3a66a
Marijn Tijhuis <marijn@studiodumbar.com>
2015-03-17 09:46:57 +0100

    Update SmoothScroll to 5.3.6.

--- assets/javascript/plugins/smoothscroll.js
+++ assets/javascript/plugins/smoothscroll.js
@@ -1,21 +1,21 @@
 /**
- * smooth-scroll v5.3.3
+ * smooth-scroll v5.3.6
  * Animate scrolling to anchor links, by Chris Ferdinandi.
  * http://github.com/cferdinandi/smooth-scroll
- * 
+ *
  * Free to use under the MIT License.
  * http://gomakethings.com/mit/
  */
 
 (function (root, factory) {
 	if ( typeof define === 'function' && define.amd ) {
-		define('smoothScroll', factory(root));
+		define([], factory(root));
 	} else if ( typeof exports === 'object' ) {
 		module.exports = factory(root);
 	} else {
 		root.smoothScroll = factory(root);
 	}
-})(window || this, function (root) {
+})(this, function (root) {
 
 	'use strict';
 
@@ -25,7 +25,7 @@
 
 	var smoothScroll = {}; // Object for public APIs
 	var supports = !!document.querySelector && !!root.addEventListener; // Feature test
-	var settings, eventTimeout, fixedHeader;
+	var settings, eventTimeout, fixedHeader, headerHeight;
 
 	// Default settings
 	var defaults = {
@@ -269,6 +269,10 @@
 		}
 	};
 
+	var getHeaderHeight = function ( header ) {
+		return header === null ? 0 : ( getHeight( header ) + header.offsetTop );
+	};
+
 	/**
 	 * Start/stop the scrolling animation
 	 * @public
@@ -288,7 +292,7 @@
 		var anchorElem = anchor === '#' ? document.documentElement : document.querySelector(anchor);
 		var startLocation = root.pageYOffset; // Current location on the page
 		if ( !fixedHeader ) { fixedHeader = document.querySelector('[data-scroll-header]'); }  // Get the fixed header if not already set
-		var headerHeight = fixedHeader === null ? 0 : ( getHeight( fixedHeader ) + fixedHeader.offsetTop ); // Get the height of a fixed header if one exists
+		if ( !headerHeight ) { headerHeight = getHeaderHeight( fixedHeader ); } // Get the height of a fixed header if one exists and not already set
 		var endLocation = getEndLocation( anchorElem, headerHeight, parseInt(settings.offset, 10) ); // Scroll to location
 		var animationInterval; // interval timer
 		var distance = endLocation - startLocation; // distance to travel
@@ -372,7 +376,7 @@
 		if ( !eventTimeout ) {
 			eventTimeout = setTimeout(function() {
 				eventTimeout = null; // Reset timeout
-				headerHeight = fixedHeader === null ? 0 : ( getHeight( fixedHeader ) + fixedHeader.offsetTop ); // Get the height of a fixed header if one exists
+				headerHeight = getHeaderHeight( fixedHeader ); // Get the height of a fixed header if one exists
 			}, 66);
 		}
 	};
@@ -394,6 +398,7 @@
 		settings = null;
 		eventTimeout = null;
 		fixedHeader = null;
+		headerHeight = null;
 	};
 
 	/**
@@ -412,6 +417,7 @@
 		// Selectors and variables
 		settings = extend( defaults, options || {} ); // Merge user options with defaults
 		fixedHeader = document.querySelector('[data-scroll-header]'); // Get the fixed header
+		headerHeight = getHeaderHeight( fixedHeader );
 
 		// When a toggle is clicked, run the click handler
 		document.addEventListener('click', eventHandler, false );
