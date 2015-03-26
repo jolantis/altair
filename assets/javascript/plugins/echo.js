8817f56a391bedae310e4273959db16cd00bdfd7
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-03-26 09:35:30 +0100

    Fix handling of hidden images (echo.js).

--- assets/javascript/plugins/echo.js
+++ assets/javascript/plugins/echo.js
@@ -20,7 +20,15 @@
 
 	var offset, poll, delay, useDebounce, unload, selector;
 
+	var isHidden = function (element) {
+		return (element.offsetParent === null);
+	};
+
 	var inView = function (element, view) {
+		if (isHidden(element)) {
+			return false;
+		}
+
 		var box = element.getBoundingClientRect();
 		return (box.right >= view.l && box.bottom >= view.t && box.left <= view.r && box.top <= view.b);
 	};
