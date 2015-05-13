6809b4b921c2333ff80a33c227e5bd71fa4b3cc6
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-05-13 22:54:42 +0200

    Update (and hopefully) fix lazysizes bgimages…

--- assets/javascript/mobile.scripts.js
+++ assets/javascript/mobile.scripts.js
@@ -15,20 +15,6 @@ domready(function () {
 	// 	resrc.configure(custom_resrc).run();
 	// }
 
-	// LazySizes support for background images
-	addEventListener('lazybeforeunveil', function(e) {
-		var bg = e.target.getAttribute('data-bg');
-		if(bg){
-			e.target.style.backgroundImage = 'url(' + bg + ')';
-			e.target.removeAttribute('data-bg');
-		}
-	});
-
-	// LazySizes RiAS plugin (3rd/resrc party support) config
-	addEventListener('lazyriasmodifyoptions', function(event) {
-		event.detail.quality = (window.devicePixelRatio || 1) > 1.4 ? 65 : 80;
-	});
-
 	alerts.init(push_message);                // Init alerts
 	expand.init();                            // Init expand / collapse
 	navMain.init();                           // Init main navigation
@@ -38,15 +24,15 @@ domready(function () {
 
 
 // LazySizes support for background images
-// document.addEventListener('lazybeforeunveil', function(e) {
-// 	var bg = e.target.getAttribute('data-bg');
-// 	if(bg){
-// 		e.target.style.backgroundImage = 'url(' + bg + ')';
-// 		e.target.removeAttribute('data-bg');
-// 	}
-// });
+document.addEventListener('lazybeforeunveil', function(e) {
+	var bg = e.target.getAttribute('data-bg');
+	if(bg){
+		e.target.style.backgroundImage = 'url(' + bg + ')';
+		e.target.removeAttribute('data-bg');
+	}
+});
 
 // LazySizes RiAS plugin (3rd/resrc party support) config
-// document.addEventListener('lazyriasmodifyoptions', function(event) {
-// 	event.detail.quality = (window.devicePixelRatio || 1) > 1.4 ? 65 : 80;
-// });
+document.addEventListener('lazyriasmodifyoptions', function(event) {
+	event.detail.quality = (window.devicePixelRatio || 1) > 1.4 ? 65 : 80;
+});
