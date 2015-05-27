b6926324a8034070b53439bca8ebbe6a987774a0
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-05-27 11:20:33 +0200

    Add noscript fallback to figure plugin template.
    And hide all elements with `resrc` and `lazyload` classes when javascript is disabled and/or not available (`no-js`).

--- site/plugins/figure/template/figure.php
+++ site/plugins/figure/template/figure.php
@@ -10,11 +10,17 @@
 		<div class="FigureImage-lazy lazyload" style="padding-bottom: <?php echo $percentage_padding; ?>%;">
 			<img data-src="<?php echo $thumburl; ?>" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" class="FigureImage-item lazyload<?php if($class): echo ' ' . $class; endif; ?>" alt="<?php if($alt): echo $alt; endif; ?>" />
 		</div>
+		<noscript>
+			<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item<?php if($class): echo ' ' . $class; endif; ?>" alt="<?php if($alt): echo $alt; endif; ?>" />
+		</noscript>
 	<?php endif; ?>
 
 	<?php // [3] Resrc image; full size thumb (let resrc resize and optimize the biggest possible thumb!) ?>
 	<?php if($lazyload == false && c::get('resrc') == true): ?>
 		<img data-src="<?php echo 'http://' . c::get('resrc.plan') . '/' . c::get('resrc.params') . '/' . $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item resrc<?php if($class): echo ' ' . $class; endif; ?>" alt="<?php if($alt): echo $alt; endif; ?>" />
+		<noscript>
+			<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item<?php if($class): echo ' ' . $class; endif; ?>" alt="<?php if($alt): echo $alt; endif; ?>" />
+		</noscript>
 	<?php endif; ?>
 
 	<?php // [4] Lazyload + resrc image; full size thumb (let resrc resize and optimize the biggest possible thumb!) ?>
@@ -22,6 +28,9 @@
 		<div class="FigureImage-lazy lazyload" style="padding-bottom: <?php echo $percentage_padding; ?>%;">
 			<img data-sizes="auto" data-src="<?php echo 'http://' . c::get('resrc.plan') . '/s=w{width}/o={quality}/' . $thumburl; ?>" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" class="FigureImage-item lazyload<?php if($class): echo ' ' . $class; endif; ?>" alt="<?php if($alt): echo $alt; endif; ?>" />
 		</div>
+		<noscript>
+			<img src="<?php echo $thumburl; ?>" width="<?php echo $image->width(); ?>" height="<?php echo $image->height(); ?>" class="FigureImage-item<?php if($class): echo ' ' . $class; endif; ?>" alt="<?php if($alt): echo $alt; endif; ?>" />
+		</noscript>
 	<?php endif; ?>
 
 	<?php if($caption): ?>
