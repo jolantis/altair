913036a30b0983c9ad56c78410a969aa84256df1
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-04 13:53:20 +0200

    Update feed title (add site title/name).

--- site/plugins/feed/template.php
+++ site/plugins/feed/template.php
@@ -2,9 +2,9 @@
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
 
 	<channel>
-		<title><?php echo xml($title) ?></title>
 		<link><?php echo xml($link) ?></link>
 		<generator><?php echo c::get('feed.generator', 'Kirby') ?></generator>
+		<title><?php echo xml($site->title() . ' ' . $title); ?></title>
 		<lastBuildDate><?php echo $site->find($channel)->modified('r'); ?></lastBuildDate>
 		<atom:link href="<?php echo xml($url); ?>" rel="self" type="application/rss+xml" />
 
