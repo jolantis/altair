176d3ad8db3b7a6bd1a0382c9dbc997c698de6b4
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-04 13:55:20 +0200

    Add (missing) semicolons.

--- site/plugins/feed/template.php
+++ site/plugins/feed/template.php
@@ -2,22 +2,22 @@
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
 
 	<channel>
-		<generator><?php echo c::get('feed.generator', 'Kirby') ?></generator>
 		<title><?php echo xml($site->title() . ' ' . $title); ?></title>
 		<link><?php echo xml($channel); ?></link>
+		<generator><?php echo c::get('feed.generator', 'Kirby'); ?></generator>
 		<lastBuildDate><?php echo $site->find($channel)->modified('r'); ?></lastBuildDate>
 		<atom:link href="<?php echo xml($url); ?>" rel="self" type="application/rss+xml" />
 
 		<?php if(!is_null($description)): ?>
-			<description><?php echo xml($description) ?></description>
+			<description><?php echo xml($description); ?></description>
 		<?php endif ?>
 
 		<?php foreach($items as $item): ?>
 			<item>
-				<title><?php echo xml($item->title()) ?></title>
-				<link><?php echo xml($item->url()) ?></link>
-				<guid><?php echo xml($item->id()) ?></guid>
-				<pubDate><?php echo $datefield == 'modified' ? $item->modified('r') : $item->date('r', $datefield) ?></pubDate>
+				<title><?php echo xml($item->title()); ?></title>
+				<link><?php echo xml($item->url()); ?></link>
+				<guid><?php echo xml($item->id()); ?></guid>
+				<pubDate><?php echo $datefield == 'modified' ? $item->modified('r') : $item->date('r', $datefield); ?></pubDate>
 				<?php if($excerpt == true): ?>
 					<?php $excerptlenght = (isset($excerptlenght)) ? $excerptlenght : 300;  ?>
 					<description>
