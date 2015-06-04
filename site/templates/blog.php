8ce1316342b22738d39824de0a57e05b138e52c3
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-04 11:42:01 +0200

    Remove ‘loading photos from folder’ tempalte code.

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -17,32 +17,6 @@
 		<?php echo $page->intro()->kirbytext(); ?>
 		<?php echo $page->text()->kirbytext(); ?>
 
-		<!-- start: load photos from folder -->
-		<?php if($page->hasImages()) : ?>
-
-			<h2>Photo(s) form page</h2>
-
-			<?php foreach($page->images() as $image): ?>
-			<figure>
-				<?php if($image->caption()=='') {
-					$imageAlt = $image->title();
-				} else {
-					$imageAlt = '';
-				} ?>
-				<?php echo thumb($image, array('width' => 450, 'height' => 450, 'hd' => false, 'crop' => true, 'alt' => $imageAlt)); ?>
-				<!-- <img src="<?php echo $image->url(); ?>" alt="<?php echo $image->title(); ?>" /> -->
-				<!-- <img src="http://photos.artlantis.nl/2012/<?php echo $image->title(); ?>.jpg" alt="<?php echo $image->title(); ?>" /> -->
-				<?php if($image->caption()!='') : ?>
-				<figcaption>
-					<p><?php echo $image->caption(); ?></p>
-				</figcaption>
-				<?php endif; ?>
-			</figure>
-			<?php endforeach; ?>
-
-		<?php endif; ?>
-		<!-- end -->
-
 		<?php snippet('nav_sub'); ?>
 
 	</main>
