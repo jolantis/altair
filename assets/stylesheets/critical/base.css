80cce3e6e33bab9e8b1d3cf0903065590bb8a783
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-05 17:01:19 +0200

    Build v0.3.4.

--- assets/stylesheets/critical/base.css
+++ assets/stylesheets/critical/base.css
@@ -1,5 +1,5 @@
 /**
- * Altair — A Sass- and Grunt-based development front-end starter kit, running on file-based Kirby CMS — v0.3.3 — 2015-06-05
+ * Altair — A Sass- and Grunt-based development front-end starter kit, running on file-based Kirby CMS — v0.3.4 — 2015-06-05
  * http://altair.studiodumbar.info
  * Copyright (c) 2015 Studio Dumbar
  */
