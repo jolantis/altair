2b3b554332e8823d2012e10250c73ff598061620
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-05 11:52:33 +0200

    Add figure plugin example to blog post tempalte (for testing).

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -22,32 +22,17 @@
 			<?php echo $page->text()->kirbytext(); ?>
 
 			<!-- start: load photos from folder -->
-			<?php /*
 			<?php if($page->hasImages()) : ?>
 
 				<h2>Photo(s) form page</h2>
 
 				<?php foreach($page->images() as $image): ?>
-				<figure>
-					<?php if($image->caption()=='') {
-						$imageAlt = $image->title();
-					} else {
-						$imageAlt = '';
-					} ?>
-					<?php echo thumb($image, array('width' => 450, 'height' => 450, 'hd' => false, 'crop' => true, 'alt' => $imageAlt)); ?>
-					<!-- <img src="<?php echo $image->url(); ?>" alt="<?php echo $image->title(); ?>" /> -->
-					<!-- <img src="http://photos.artlantis.nl/2012/<?php echo $image->title(); ?>.jpg" alt="<?php echo $image->title(); ?>" /> -->
-					<?php if($image->caption()!='') : ?>
-					<figcaption>
-						<p><?php echo $image->caption(); ?></p>
-					</figcaption>
-					<?php endif; ?>
-				</figure>
+					<?php $caption = $image->caption() == '' ? $image->title() : ''; ?>
+					<?php echo figure($image, array('caption' => $caption)); ?>
 				<?php endforeach; ?>
 
 			<?php endif; ?>
 			<!-- end -->
-			*/ ?>
 
 			<?php snippet('share_page'); ?>
 
