a744dbd6fd89e4ad61bfce578eb109711f1cfb2a
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-07 14:43:41 +0200

    Add code to replace not only `data-src`, but also `data-bgset` lazysizes width and quality values with calculated ones.

--- assets/javascript/classes/lazysizes.class.js
+++ assets/javascript/classes/lazysizes.class.js
@@ -16,10 +16,17 @@ var lazysizes = {
 			e.detail.quality = (window.devicePixelRatio || 1) > 1.4 ? 65 : 60;
 		});
 		document.addEventListener('lazybeforeunveil', function(e){
-			// Replace default `data-src` placeholder {width} and {quality} with calculated values
-			var src = e.target.getAttribute('data-src');
-			if(!src) { return; }
-			e.target.setAttribute('data-src', src.replace(/s=w(\d+)/, 's=w{width}').replace(/o=(\d+)/, 'o={quality}'));
+			// Replace default `data-src/bgset` placeholder width and quality
+			// values, with the by lazyizes calculated {width} and {quality} values
+			var srcAttr = 'data-src';
+			var src = e.target.getAttribute(srcAttr);
+			if(!src) {
+				srcAttr = 'data-bgset';
+				src = e.target.getAttribute(srcAttr);
+				if(!src) { return; }
+			}
+			e.target.setAttribute(srcAttr, src.replace(/s=w(\d+)/, 's=w{width}').replace(/o=(\d+)/, 'o={quality}'));
+
 			// // Basic support for background images (for without bgset plugin)
 			// var bg = e.target.getAttribute('data-bg');
 			// if(bg) {
