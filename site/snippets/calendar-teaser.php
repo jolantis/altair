5b02565cfc5518ec80f4f512f27b2ca51cc33fbf
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-06-12 14:03:59 +0200

    Clean-up lines.

--- site/snippets/calendar-teaser.php
+++ site/snippets/calendar-teaser.php
@@ -1,12 +1,10 @@
 <ul class="teaser">
-<?php
-	foreach ($calendar->getEvents() as $event):
-		if (--$items < 0) break;
-?>
-	<li><strong><?php echo $event->getBeginHtml(); ?></strong><?php
-		foreach ($fields as $key => $value) {
-			echo ' '.$event->getField($key);
-		}
-	?></li>
+<?php foreach ($calendar->getEvents() as $event): ?>
+	<?php if (--$items < 0) break; ?>
+	<li><strong><?php echo $event->getBeginHtml(); ?></strong>
+		<?php foreach ($fields as $key => $value): ?>
+			<?php echo ' '.$event->getField($key); ?>
+		<?php endforeach; ?>
+	</li>
 <?php endforeach; ?>
 </ul>
