f446d0d611242e374a7f6b80393bc84d31c88b33
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-07-24 12:03:45 +0200

    Add missing id="PageTop" to banner.

--- site/snippets/banner.php
+++ site/snippets/banner.php
@@ -4,7 +4,7 @@
 // ----------------------------------------------------------
 ////////////////////////////////////////////////////////// ?>
 
-<header role="banner" class="Banner">
+<header role="banner" id="PageTop" class="Banner">
 	<?php if($page->isHomePage()): echo '<span'; else: echo '<a href="' . $site->url() . '" title="Return to the homepage" rel="home"'; endif; echo ' class="Masthead">'?>
 		<img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Altair"/>
 		<?php // echo $site->title(); ?>
