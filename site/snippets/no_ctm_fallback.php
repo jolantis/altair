7f5ab6fdc0d82a46a666f2e1765be1703ec0b914
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-07-24 09:45:21 +0200

    Add minified verison, and add some more comments.

--- site/snippets/no_ctm_fallback.php
+++ site/snippets/no_ctm_fallback.php
@@ -6,41 +6,33 @@
 // ----------------------------------------------------------
 ///////////////////////////////////////////////////////////// ?>
 
-<script>
-// function cutsTheMustard() {
-// 	if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
-// 		return true;
-// 	}
-// 	else {
-// 		return false;
-// 	}
-// }
-
-if(enhance.ctm() === false) {
-	var lazyloadimgs = document.querySelectorAll('img.lazyload');
-
-	for(i = 0; i < lazyloadimgs.length; i++) {
-		// Set data-src as src
-		var datasrc = lazyloadimgs[i].getAttribute('data-src');
-		lazyloadimgs[i].setAttribute('src', datasrc);
-
-		// Set/undo some no-js styles to show images nicely
-		lazyloadimgs[i].style.display = 'block';
-		lazyloadimgs[i].parentNode.style.display = 'block';
-		lazyloadimgs[i].parentNode.style.paddingBottom = '0px';
-	}
-
-	var lazyloaddivs = document.querySelectorAll('[data-bgset]');
-	for(i = 0; i < lazyloaddivs.length; i++) {
-		// Set data-bgset as style background
-		var datasrc = lazyloaddivs[i].getAttribute('data-bgset');
-		lazyloaddivs[i].style.backgroundImage = 'url('+datasrc+')';
-	}
-}
-</script>
+<script>if(enhance.ctm()===!1){var lazyloadimgs=document.querySelectorAll("img.lazyload");for(i=0;i<lazyloadimgs.length;i++){var datasrc=lazyloadimgs[i].getAttribute("data-src");lazyloadimgs[i].setAttribute("src",datasrc),lazyloadimgs[i].style.display="block",lazyloadimgs[i].parentNode.style.display="block",lazyloadimgs[i].parentNode.style.paddingBottom="0px"}var lazyloaddivs=document.querySelectorAll("[data-bgset]");for(i=0;i<lazyloaddivs.length;i++){var datasrc=lazyloaddivs[i].getAttribute("data-bgset");lazyloaddivs[i].style.backgroundImage="url("+datasrc+")"}}</script>
 
 <?php /* ///////////////////// UNMINIFIED! /////////////////////
 <script>
-	unminified here!
+	if(enhance.ctm() === false) {
+		// Display ‘lazyloaded’ images with default width and quality,
+		// see: `thumbs.width.default` and `thumbs.quality` in config
+		var lazyloadimgs = document.querySelectorAll('img.lazyload');
+		for(i = 0; i < lazyloadimgs.length; i++) {
+			// Set data-src as src
+			var datasrc = lazyloadimgs[i].getAttribute('data-src');
+			lazyloadimgs[i].setAttribute('src', datasrc);
+
+			// Set/undo some no-js styles to show images nicely
+			lazyloadimgs[i].style.display = 'block';
+			lazyloadimgs[i].parentNode.style.display = 'block';
+			lazyloadimgs[i].parentNode.style.paddingBottom = '0px';
+		}
+
+		// Display ‘lazyloaded’ background images with default width and quality,
+		// see: `thumbs.width.default` and `thumbs.quality` in config
+		var lazyloaddivs = document.querySelectorAll('[data-bgset]');
+		for(i = 0; i < lazyloaddivs.length; i++) {
+			// Set data-bgset as style background
+			var datasrc = lazyloaddivs[i].getAttribute('data-bgset');
+			lazyloaddivs[i].style.backgroundImage = 'url('+datasrc+')';
+		}
+	}
 </script>
 ////////////////////////////////////////////////////////// */ ?>
