e8e4f590c4fe8d6a474151590b411476a51b19a3
Jonathan van Wunnik <jonathan@artlantis.nl>
2015-08-25 10:32:19 +0200

    Add site title before (rss) feed title.

--- site/plugins/feed/feed.php
+++ site/plugins/feed/feed.php
@@ -12,7 +12,7 @@ Pages::$methods['feed'] = function($pages, $params = array()) {
 	// set all default values
 	$defaults = array(
 		'url'         => url(),
-		'title'       => page()->title(),
+		'title'       => site()->title() . ': ' . page()->title(),
 		'description' => page()->description(),
 		'link'        => url(),
 		'datefield'   => 'date',
