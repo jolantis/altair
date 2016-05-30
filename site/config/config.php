375826057c22613bd2f60525aef1584a32bc4d8b
Jonathan van Wunnik <jonathan@artlantis.nl>
2016-05-30 12:29:16 +0200

    Delete deprecated `thumbs.width.feed` variable.

--- site/config/config.php
+++ site/config/config.php
@@ -364,7 +364,6 @@ Thumbs
 c::set('thumbs.driver', 'im');                                                  // The thumbnail library which is being used by Kirby's thumb function/class ('gd' or 'im')
 // c::set('thumbs.filename', '{safeName}-{hash}.{extension}');                     // The naming scheme for thumb files (default is: {safeName}-{hash}.{extension})
 c::set('thumbs.quality', 78);                                                   // The default jpg compression (or quality setting) for thumbs (figure plugin and tag);
-c::set('thumbs.width.feed', 1600);                                              // An arbitrary width for thumbs used in feeds (via figure tag)
 
 
 /* -----------------------------------------------------------------------------
