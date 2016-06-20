2681a6f1d8097869d549c20f1dd026fb1173df79
Marijn Tijhuis <post@fatpixel.nl>
2016-06-20 16:29:17 +0200

    Re-arrange javascript to more clear file struture.

--- gruntfile.js
+++ gruntfile.js
@@ -18,11 +18,10 @@ module.exports = function(grunt) {
 			scripts: '<%= project.assets %>/javascript',
 			scripts_dev: '<%= project.scripts %>/dev',
 			scripts_min: '<%= project.scripts %>/min',
-			scripts_classes: '<%= project.scripts %>/classes',
-			scripts_plugins: '<%= project.scripts %>/plugins',
-			scripts_polyfills: '<%= project.scripts %>/polyfills',
-			scripts_utils: '<%= project.scripts %>/utils',
-			scripts_vendor: '<%= project.scripts %>/vendor',
+			scripts_modules: '<%= project.scripts %>/modules',
+			scripts_plugins: '<%= project.scripts %>/lib/plugins',
+			scripts_polyfills: '<%= project.scripts %>/lib/polyfills',
+			scripts_utils: '<%= project.scripts %>/lib/utils',
 		},
 
 		// Project banner
@@ -47,103 +46,102 @@ module.exports = function(grunt) {
 		// JS files and order
 		jsfiles: {
 			head: [
-				'<%= project.scripts_vendor %>/enhance.js',
-				'<%= project.scripts_vendor %>/modernizr.dev.js',
-				// '<%= project.scripts_vendor %>/modernizr.min.js',               // ALWAYS use custom build modernizr in production!
+				'<%= project.scripts_plugins %>/enhance/enhance.js',
+				'<%= project.scripts_plugins %>/modernizr/modernizr.dev.js',
+				// '<%= project.scripts_plugins %>/modernizr/modernizr.min.js',               // ALWAYS use custom build modernizr in production!
 				'<%= project.scripts %>/head.scripts.js',
 			],
 			main: {
+				plugins: [
+					// '<%= project.scripts_plugins %>/domdelegate/domdelegate.js',
+					'<%= project.scripts_plugins %>/domready/domready.js',
+					'<%= project.scripts_plugins %>/fontfaceobserver/fontfaceobserver.js',
+					'<%= project.scripts_plugins %>/lazysizes/lazysizes.min.js',           // Out-comment when using lazyload
+					// '<%= project.scripts_plugins %>/lazysizes/ls.respimg.js',             // Out-comment when using lazyload, and need to polyfill responsive images
+					'<%= project.scripts_plugins %>/lazysizes/ls.bgset.js',               // Out-comment when using lazyload + multiple background images with a width descriptor, similar to how img[srcset] works
+					'<%= project.scripts_plugins %>/lazysizes/ls.parent-fit.js',          // Out-comment when using lazyload + bgset, to calculate the right sizes for object-fit: contain|cover image elements
+					// '<%= project.scripts_plugins %>/lazysizes/ls.unload.js',              // Out-comment when using lazyload, and want to unload not in view images to improve memory consumption and orientationchange/resize performance
+					// '<%= project.scripts_plugins %>/lazysizes/ls.unveilhooks.js',         // Out-comment when using lazyload + unveil/lazyload scripts/widgets, background images, styles and video/audio elements
+					'<%= project.scripts_plugins %>/lazysizes/ls.print.js',               // Out-comment when there's a need to be able to print lazyloaded images
+					'<%= project.scripts_plugins %>/transitionend/transitionend.js',
+					'<%= project.scripts_plugins %>/smooth-scroll/smooth-scroll.js',
+					// '<%= project.scripts_plugins %>/gumshoe/gumshoe.js',
+				],
 				polyfills: [
 					'<%= project.scripts_polyfills %>/classlist.js',
-					'<%= project.scripts_polyfills %>/domready.js',
-				],
-				plugins: [
-					// '<%= project.scripts_plugins %>/domdelegate.js',
-					'<%= project.scripts_vendor %>/fontfaceobserver.js',
-					'<%= project.scripts_vendor %>/lazysizes.min.js',           // Out-comment when using lazyload
-					// '<%= project.scripts_plugins %>/ls.respimg.js',             // Out-comment when using lazyload, and need to polyfill responsive images
-					'<%= project.scripts_plugins %>/ls.bgset.js',               // Out-comment when using lazyload + multiple background images with a width descriptor, similar to how img[srcset] works
-					'<%= project.scripts_plugins %>/ls.parent-fit.js',          // Out-comment when using lazyload + bgset, to calculate the right sizes for object-fit: contain|cover image elements
-					// '<%= project.scripts_plugins %>/ls.unload.js',              // Out-comment when using lazyload, and want to unload not in view images to improve memory consumption and orientationchange/resize performance
-					// '<%= project.scripts_plugins %>/ls.unveilhooks.js',         // Out-comment when using lazyload + unveil/lazyload scripts/widgets, background images, styles and video/audio elements
-					'<%= project.scripts_plugins %>/ls.print.js',               // Out-comment when there's a need to be able to print lazyloaded images
-					'<%= project.scripts_plugins %>/transitionend.js',
-					'<%= project.scripts_plugins %>/smooth-scroll.js',
-					// '<%= project.scripts_plugins %>/gumshoe.js',
-
 				],
 				utils: [
-					'<%= project.scripts_utils %>/extend.util.js',
-					'<%= project.scripts_utils %>/alerts.util.js',
 					// '<%= project.scripts_utils %>/ajax.util.js',
 					// '<%= project.scripts_utils %>/cookie.util.js',           // Out-comment when enhance (and thus enhance.cookie) is not used
 					'<%= project.scripts_utils %>/domparents.util.js',
+					'<%= project.scripts_utils %>/extend.util.js',
 				],
 				other: [
-				// Classes
-					'<%= project.scripts_classes %>/expand.class.js',
-					// '<%= project.scripts_classes %>/lazysizes.class.js',     // Out-comment when apply custom actions on lazysizes init (also out-comment `lazysizes.init();` in main and mobile scripts)
-					'<%= project.scripts_classes %>/fontobserver.class.js',
-					'<%= project.scripts_classes %>/navmain.class.js',
-					'<%= project.scripts_classes %>/popup.class.js',
+				// Modules
+					'<%= project.scripts_modules %>/alerts.module.js',
+					'<%= project.scripts_modules %>/expand.module.js',
+					// '<%= project.scripts_modules %>/lazysizes.module.js',     // Out-comment when apply custom actions on lazysizes init (also out-comment `lazysizes.init();` in main and mobile scripts)
+					'<%= project.scripts_modules %>/fontobserver.module.js',
+					'<%= project.scripts_modules %>/navmain.module.js',
+					'<%= project.scripts_modules %>/popup.module.js',
 				// Main
 					'<%= project.scripts %>/main.scripts.js',
 				],
 			},
 			mobile: {
+				plugins: [
+					// '<%= project.scripts_plugins %>/domdelegate/domdelegate.js',
+					'<%= project.scripts_polyfills %>/domready/domready.js',
+					'<%= project.scripts_plugins %>/fontfaceobserver/fontfaceobserver.js',
+					'<%= project.scripts_plugins %>/lazysizes/lazysizes.min.js',           // Out-comment when using lazyload
+					// '<%= project.scripts_plugins %>/lazysizes/ls.respimg.js',             // Out-comment when using lazyload, and need to polyfill responsive images
+					'<%= project.scripts_plugins %>/lazysizes/ls.bgset.js',               // Out-comment when using lazyload + multiple background images with a width descriptor, similar to how img[srcset] works
+					'<%= project.scripts_plugins %>/lazysizes/ls.parent-fit.js',          // Out-comment when using lazyload + bgset, to calculate the right sizes for object-fit: contain|cover image elements
+					// '<%= project.scripts_plugins %>/lazysizes/ls.unload.js',              // Out-comment when using lazyload, and want to unload not in view images to improve memory consumption and orientationchange/resize performance
+					// '<%= project.scripts_plugins %>/lazysizes/ls.unveilhooks.js',         // Out-comment when using lazyload + unveil/lazyload scripts/widgets, background images, styles and video/audio elements
+					'<%= project.scripts_plugins %>/lazysizes/ls.print.js',               // Out-comment when there's a need to be able to print lazyloaded images
+					'<%= project.scripts_plugins %>/transitionend/transitionend.js',
+					'<%= project.scripts_plugins %>/smooth-scroll/smooth-scroll.js',
+				],
 				polyfills: [
 					'<%= project.scripts_polyfills %>/classlist.js',
-					'<%= project.scripts_polyfills %>/domready.js',
-				],
-				plugins: [
-					// '<%= project.scripts_plugins %>/domdelegate.js',
-					'<%= project.scripts_vendor %>/fontfaceobserver.js',
-					'<%= project.scripts_vendor %>/lazysizes.min.js',           // Out-comment when using lazyload
-					// '<%= project.scripts_plugins %>/ls.respimg.js',             // Out-comment when using lazyload, and need to polyfill responsive images
-					'<%= project.scripts_plugins %>/ls.bgset.js',               // Out-comment when using lazyload + multiple background images with a width descriptor, similar to how img[srcset] works
-					'<%= project.scripts_plugins %>/ls.parent-fit.js',          // Out-comment when using lazyload + bgset, to calculate the right sizes for object-fit: contain|cover image elements
-					// '<%= project.scripts_plugins %>/ls.unload.js',              // Out-comment when using lazyload, and want to unload not in view images to improve memory consumption and orientationchange/resize performance
-					// '<%= project.scripts_plugins %>/ls.unveilhooks.js',         // Out-comment when using lazyload + unveil/lazyload scripts/widgets, background images, styles and video/audio elements
-					'<%= project.scripts_plugins %>/ls.print.js',               // Out-comment when there's a need to be able to print lazyloaded images
-					'<%= project.scripts_plugins %>/transitionend.js',
-					'<%= project.scripts_plugins %>/smooth-scroll.js',
 				],
 				utils: [
-					'<%= project.scripts_utils %>/extend.util.js',
-					'<%= project.scripts_utils %>/alerts.util.js',
 					// '<%= project.scripts_utils %>/ajax.util.js',
 					// '<%= project.scripts_utils %>/cookie.util.js',           // Out-comment when enhance (and thus enhance.cookie) is not used
 					'<%= project.scripts_utils %>/domparents.util.js',
+					'<%= project.scripts_utils %>/extend.util.js',
 				],
 				other: [
-				// Classes
-					'<%= project.scripts_classes %>/expand.class.js',
-					'<%= project.scripts_classes %>/fontobserver.class.js',
-					// '<%= project.scripts_classes %>/lazysizes.class.js',     // Out-comment when apply custom actions on lazysizes init (also out-comment `lazysizes.init();` in main and mobile scripts)
-					'<%= project.scripts_classes %>/navmain.class.js',
+				// Modules
+					'<%= project.scripts_modules %>/alerts.module.js',
+					'<%= project.scripts_modules %>/expand.module.js',
+					// '<%= project.scripts_modules %>/lazysizes.module.js',     // Out-comment when apply custom actions on lazysizes init (also out-comment `lazysizes.init();` in main and mobile scripts)
+					'<%= project.scripts_modules %>/fontobserver.module.js',
+					'<%= project.scripts_modules %>/navmain.module.js',
 				// Main
 					'<%= project.scripts %>/mobile.scripts.js',
 				],
 			},
 			photoswipe: {
+				plugins: [
+					'<%= project.scripts_plugins %>/photoswipe/pswp.startfile.js',              // Photoswipe **required**, always first!
+					'<%= project.scripts_plugins %>/photoswipe/pswp.framework-bridge.js',       // Photoswipe **required**
+					'<%= project.scripts_plugins %>/photoswipe/pswp.core.js',                   // Photoswipe **required**
+					'<%= project.scripts_plugins %>/photoswipe/pswp.gestures.js',               // Photoswipe **required**
+					'<%= project.scripts_plugins %>/photoswipe/pswp.show-hide-transition.js',   // Photoswipe **required**
+					'<%= project.scripts_plugins %>/photoswipe/pswp.items-controller.js',       // Photoswipe **required**
+					'<%= project.scripts_plugins %>/photoswipe/pswp.tap.js',                    // Photoswipe optional
+					'<%= project.scripts_plugins %>/photoswipe/pswp.history.js',                // Photoswipe optional
+					'<%= project.scripts_plugins %>/photoswipe/pswp.endfile.js',                // Photoswipe **required**, always last!
+					'<%= project.scripts_plugins %>/photoswipe/pswp.photoswipe-ui-default.js',  // Photoswipe UI **required**
+				],
 				polyfills: [
 					'<%= project.scripts_polyfills %>/domready.js',
 				],
-				plugins: [
-					'<%= project.scripts_plugins %>/pswp.startfile.js',              // Photoswipe **required**, always first!
-					'<%= project.scripts_plugins %>/pswp.framework-bridge.js',       // Photoswipe **required**
-					'<%= project.scripts_plugins %>/pswp.core.js',                   // Photoswipe **required**
-					'<%= project.scripts_plugins %>/pswp.gestures.js',               // Photoswipe **required**
-					'<%= project.scripts_plugins %>/pswp.show-hide-transition.js',   // Photoswipe **required**
-					'<%= project.scripts_plugins %>/pswp.items-controller.js',       // Photoswipe **required**
-					'<%= project.scripts_plugins %>/pswp.tap.js',                    // Photoswipe optional
-					'<%= project.scripts_plugins %>/pswp.history.js',                // Photoswipe optional
-					'<%= project.scripts_plugins %>/pswp.endfile.js',                // Photoswipe **required**, always last!
-					'<%= project.scripts_plugins %>/pswp.photoswipe-ui-default.js',  // Photoswipe UI **required**
-				],
 				other: [
-				// Classes
-					'<%= project.scripts_classes %>/photoswipe.class.js',
+				// Modules
+					'<%= project.scripts_modules %>/photoswipe.module.js',
 				// Main
 					'<%= project.scripts %>/photoswipe.scripts.js',
 				],
