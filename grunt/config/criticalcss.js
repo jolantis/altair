36cb0c8863a54c3e8ef78a1411bbceab64ed28f9
Jonathan van Wunnik <jonathan@artlantis.nl>
2016-09-16 16:41:12 +0200

    Update Grunt criticallcss config.
    Add default criticallcss to config.

--- grunt/config/criticalcss.js
+++ grunt/config/criticalcss.js
@@ -1,6 +1,28 @@
 module.exports = function(grunt) {
 
 	grunt.config('criticalcss', {
+		default : {
+			options: {
+				url: 'http://local.altair',
+				width: 1440,
+				height: 900,
+				outputfile: '<%= project.styles_critical %>/default.css',
+				filename: '<%= project.styles_dev %>/main.dev.css',
+				forceInclude: [], // An array of selectors that you want to guarantee will make it from the CSS file into your CriticalCSS output.
+				buffer: 800*1024,
+			},
+		},
+		default_mobile : {
+			options: {
+				url: 'http://local.altair',
+				width: 420, // iPhone 6 Plus is 414 points wide
+				height: 960, //iPhone 6 Plus is 736 points high
+				outputfile: '<%= project.styles_critical %>/default_mobile.css',
+				filename: '<%= project.styles_dev %>/main.dev.css',
+				forceInclude: [], // An array of selectors that you want to guarantee will make it from the CSS file into your CriticalCSS output.
+				buffer: 800*1024,
+			},
+		},
 		home : {
 			options: {
 				url: 'http://local.altair',
