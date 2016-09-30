e4ded69710b539c225f7d4a91b75c8d7aa5da932
Jonathan van Wunnik <jonathan@artlantis.nl>
2016-09-30 15:59:41 +0200

    Remove white line for consistency.

--- site/templates/sitemap.php
+++ site/templates/sitemap.php
@@ -26,6 +26,7 @@ else {
 		$lang_codes[$i]['locale'] = $language->locale();
 	$i++; endforeach;
 }
+
 ////////////////////////////////////////////////////////// ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.google.com/schemas/sitemap/0.84 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"<?php if(c::get('images.in.sitemap')): ?> xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"<?php endif; ?>>
