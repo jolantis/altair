0c6a08d96f00d062e565008c5ea5b4bd10093c2d
Jonathan van Wunnik <jonathan@artlantis.nl>
2016-10-25 14:14:38 +0200

    Add panel install security.

--- site/config/config.php
+++ site/config/config.php
@@ -257,6 +257,19 @@ react to the locale settings
 
 
 /* -----------------------------------------------------------------------------
+Panel (install)
+--------------------------------------------------------------------------------
+
+For security reasons, the Panel installer can only be run on localhost.
+If you want to run the installer on a remote server, you have to set
+the `panel.install` option to true to be able to install.
+
+*/
+
+c::set('panel.install', false);
+
+
+/* -----------------------------------------------------------------------------
 Roles
 --------------------------------------------------------------------------------
 
