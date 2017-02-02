e9d6ada38bd5f424e2da2eb17ac2cd0fc58d7a51
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-02-02 09:56:29 +0100

    Fix `humans.txt` url.
    Was missing php’s `echo` to output the site url!

--- site/snippets/html-head.php
+++ site/snippets/html-head.php
@@ -78,7 +78,7 @@ $fontobserver = (isset($_COOKIE['fonts-loaded']) && $_COOKIE['fonts-loaded'] == 
 
 	<link rel="home" href="<?php echo $site->url(); ?>">
 	<?php if(c::get('tinyurl.enabled') && !$page->isHomepage()): ?><link rel="shortlink" href="<?php echo $page->tinyurl(); ?>"><?php endif; ?><?php // Shortlink, enable tinyurl in config.php ?>
-	<link rel="author" href="<?php $site->url(); ?>/humans.txt">
+	<link rel="author" href="<?php echo $site->url(); ?>humans.txt">
 	<link rel="sitemap" type="application/xml" title="<?php echo $site->title()->smartypants(); ?>: Sitemap" href="<?php echo (c::get('url') != '/') ? $site->url() . '/sitemap.xml' :  '/sitemap.xml'; ?>">
 	<?php if(page('blog')): ?><link rel="alternate" type="application/rss+xml" title="<?php echo $site->title()->smartypants(); ?>: <?php echo page('blog')->title()->smartypants(); ?> Feed" href="<?php echo (c::get('url') != '/') ? $site->url() . '/blog.rss' : '/blog.rss'; ?>"><?php endif; ?>
 
