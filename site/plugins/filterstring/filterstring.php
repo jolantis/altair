7c71646f35059b59e6b5cdae03b5120b51077528
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-06-02 13:41:05 +0200

    Cleanup comments.

--- site/plugins/filterstring/filterstring.php
+++ site/plugins/filterstring/filterstring.php
@@ -14,20 +14,6 @@
  * @version     1.0.0
  */
 
-// Filter to exclude files that contain specific string in their name
-// -----
-// You can write this to include files that contain a specific string:
-// $page->images()->filterBy('filename','*=','cover')->first();
-// -----
-// But you have to write all of this to exclude files that contain a specific string:
-// $page->images()->filterBy('filename', '!=', $page->images()->filterBy('filename','*=','cover')->first()->filename());
-// -----
-// With this plugin you can also write this to exclude files that contain a specific string:
-// $page->images()->filterBy('filename', '!*=', 'cover');
-// -----
-// See: https://forum.getkirby.com/t/filter-to-exclude-files-that-contain-specific-string-in-their-name/573
-// -----
-
 collection::$filters['!*='] = function($collection, $field, $value, $split = false) {
 
 	foreach($collection->data as $key => $item) {
@@ -38,4 +24,5 @@ collection::$filters['!*='] = function($collection, $field, $value, $split = fal
 	return $collection;
 
 }
+
 ?>
