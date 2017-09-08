e445664fe1079d9c9208d26c71521f57020e8c40
Marijn Tijhuis <post@fatpixel.nl>
2017-09-08 17:34:03 +0200

    Add SVG4everybody and Respimage polyfills for browsers that need it.

--- assets/javascript/head.scripts.js
+++ assets/javascript/head.scripts.js
@@ -4,3 +4,13 @@
  * Essential scripts, to be loaded in the head of the document
  * Use gruntfile.js to include the necessary script files.
  */
+
+// Load respimage if <picture> element is not supported
+if(!window.HTMLPictureElement){
+	enhance.loadJS('/assets/javascript/lib/polyfills/respimage.min.js');
+}
+
+// Load SVG 4 everybody, be sure to check SVG support in Modernizr
+if (!Modernizr.svg) {
+	enhance.loadJS('/assets/javascript/lib/polyfills/svg4everybody.min.js');
+}
