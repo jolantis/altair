62e5ad2e44dc689b7f5e5dbd9cf5f7814ee5f841
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-09-09 17:17:03 +0200

    Update oldie message.
    Display to IE 9 and below. Conditional comments are not supported in IE 10 and 11.

--- site/snippets/oldie-message.php
+++ site/snippets/oldie-message.php
@@ -2,8 +2,8 @@
 // ----------------------------------------------------------
 // SNIPPET
 // ----------------------------------------------------------
-// Display message to IE < 8 users!
+// Display message to IE < 10 users!
 // ----------------------------------------------------------
 ////////////////////////////////////////////////////////// ?>
 
-<!--[if lt IE 8]><p class="oldie-message">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
+<!--[if lte IE 9]><p class="oldie-message">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
