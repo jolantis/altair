d0337c801ae406538f98e9503449baaab12507f9
Marijn Tijhuis <post@fatpixel.nl>
2017-09-29 15:58:55 +0200

    Create hidden picture element when respimage is loaded.
    Seen in this example: https://github.com/fabianmichael/kirby-imageset#23-template-setup

--- assets/javascript/head.scripts.js
+++ assets/javascript/head.scripts.js
@@ -7,5 +7,6 @@
 
 // Load respimage if <picture> element is not supported
 if(!window.HTMLPictureElement){
+	document.createElement('picture');
 	enhance.loadJS('/assets/javascript/lib/polyfills/respimage.min.js');
 }
