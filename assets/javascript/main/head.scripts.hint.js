1f49d553c5b5222ddcee1ffde886a1cbfebd059f
Jonathan van Wunnik <jonathan@artlantis.nl>
2017-11-22 12:34:29 +0100

    Untrack (remove) non-minified styles and scripts!
    Only commit and track minified files (e.g. main.min.css)!

--- assets/javascript/main/head.scripts.hint.js
+++
@@ -1,8 +0,0 @@
-/**
- * For JSHint only!
- */
-// Load respimage if <picture> element is not supported
-if(!window.HTMLPictureElement) {
-	document.createElement('picture');
-	enhance.loadJS('/assets/javascript/lib/polyfills/respimage.min.js');
-}
