7c7285373eabdf89fc4920bcba809ce9192e63f0
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-03-05 10:46:35 +0100

    Fix issue with https and Google analytics tracking code.
    Use `str::after($string, $after);` to always get the (sub)domain without `http://` or `https://`.

--- site/snippets/google-analytics.php
+++ site/snippets/google-analytics.php
@@ -13,8 +13,8 @@
 // ----------------------------------------------------------
 if (c::get('google.analytics') == true && c::get('google.analytics.id') != 'TRACKING ID IS NOT SET'): ?>
 
-	<script>window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','<?php echo c::get("google.analytics.id", "TRACKING ID IS NOT SET"); ?>','<?php echo str::substr($site->url(), 7); ?>');ga('set', 'anonymizeIp', true);ga('set', 'forceSSL', true);ga('send','pageview')</script>
-	<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+		<script>window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','<?php echo c::get("google.analytics.id", "TRACKING ID IS NOT SET"); ?>','<?php echo str::after($site->url(), '://'); ?>');ga('set', 'anonymizeIp', true);ga('set', 'forceSSL', true);ga('send','pageview')</script>
+		<script src="https://www.google-analytics.com/analytics.js" async defer></script>
 
 <?php else: ?>
 
