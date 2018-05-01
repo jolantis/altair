e11f9aad17b4cbf124d2aa1194021e06c2ad8bac
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-01 14:53:57 +0200

    Fix issue with ‘respect user's privacy’ code!
    It’s Js and not PHP, stupid!

--- site/snippets/google-analytics.php
+++ site/snippets/google-analytics.php
@@ -6,20 +6,24 @@
 // ----------------------------------------------------------
 // Enable and set analytics ID/API KEY in config.php
 // ----------------------------------------------------------
+// Respect user's privacy:
+// https://blog.j15h.nu/web-analytics-privacy/
+// ----------------------------------------------------------
 /////////////////////////////////////////////////////////////
 
 // ----------------------------------------------------------
 // Google Universal Analytics
 // ----------------------------------------------------------
-if (c::get('google.analytics') == true && c::get('google.analytics.id') != 'TRACKING ID IS NOT SET'):
 
-	// Respect user's privacy (https://blog.j15h.nu/web-analytics-privacy/)
-	if ('navigator.doNotTrack' != 'yes' && 'navigator.doNotTrack' != '1' && 'navigator.msDoNotTrack' != '1'): ?>
+if (c::get('google.analytics') == true && c::get('google.analytics.id') != 'TRACKING ID IS NOT SET'): ?>
 
-		<script>window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','<?php echo c::get("google.analytics.id", "TRACKING ID IS NOT SET"); ?>','<?php echo str::after($site->url(), '://'); ?>');ga('set', 'anonymizeIp', true);ga('set', 'forceSSL', true);ga('send','pageview')</script>
-		<script src="https://www.google-analytics.com/analytics.js" async defer></script>
-
-	<?php endif; ?>
+	<script>
+		// Respect user's privacy
+		if ('navigator.doNotTrack' != 'yes' && 'navigator.doNotTrack' != '1' && 'navigator.msDoNotTrack' != '1') {
+			window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;ga('create','<?php echo c::get("google.analytics.id", "TRACKING ID IS NOT SET"); ?>','<?php echo str::after($site->url(), '://'); ?>');ga('set', 'anonymizeIp', true);ga('set', 'forceSSL', true);ga('send','pageview');
+		}
+	</script>
+	<script src="https://www.google-analytics.com/analytics.js" async defer></script>
 
 <?php else: ?>
 
