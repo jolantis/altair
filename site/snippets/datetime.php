deea59d26288a73206bc666f036f945854ff92dd
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-15 15:04:31 +0200

    Update datetime snippet to alos work with PHP's `strftime()` out of the box.
    Kirby's default date handler is `date()` and can be changed to `strftime()` in Kibry's config file. Strftime is relevant for non-english websites, where there need to be translated date and time names, which should react to the locale settings.

--- site/snippets/datetime.php
+++ site/snippets/datetime.php
@@ -6,10 +6,16 @@
 // Date: dd/mm/YYYY or Date: YYYY-mm-dd
 // more info here: http://bit.ly/I9yabi and http://bit.ly/I9y4k6
 // ----------------------------------------------------------
+/////////////////////////////////////////////////////////////
+
+// Set date time and format
+$date_format = (isset($format)) ? $format : ((c::get('date.handler') == 'strftime') ? '%G-%m-%d' : 'Y-m-d');
+$date_time   = (c::get('date.handler') == 'strftime') ? '%FT%T+00:00' : 'c';
+
 ////////////////////////////////////////////////////////// ?>
 
 <?php if($page->date($format=true)): ?>
-	<time datetime="<?php echo $page->date('c'); ?>" pubdate="Pubdate"><?php echo $page->date('Y-m-d'); ?></time>
+	<time datetime="<?php echo $page->date($date_time); ?>" pubdate="Pubdate"><?php echo $page->date('Y-m-d'); ?></time>
 <?php else: ?>
 	<p>No (correct) date field defined in content file!</p>
 <?php endif; ?>
