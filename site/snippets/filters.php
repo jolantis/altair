9b5bd894dfb6d671fe4f6a11a5cf252dd9c3cd26
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-05-15 15:06:04 +0200

    Clean-up filter code and make sure to only display filters when…
    … there are filter values either in the URL or read from cookie.

--- site/snippets/filters.php
+++ site/snippets/filters.php
@@ -2,34 +2,57 @@
 // ----------------------------------------------------------
 // SNIPPET
 // ----------------------------------------------------------
+/////////////////////////////////////////////////////////////
+
+// Get (as passed `filter_items` variable) or otherwise fetch all (visible) page items
+$page_items = (isset($filter_items)) ? $filter_items : $page->children()->visible();
+
+// Fetch filter values (e.g. tags), seperated by comma, associated to blog posts
+$filter_values = $page_items->pluck($filter_key, ',', true);
+
+// Get (as passed `sort` variable) sort order ('count' or 'abc')
+$sort = (isset($sort)) ? $sort : false;
+
+// Sort all filter values (e.g. tags) by the number of instances of each filter value
+if($sort == 'count') {
+	usort($filter_values, function($a, $b) use($page_items, $filter_key) {
+		$aCount = $page_items->filterBy($filter_key, $a, ',')->count();
+		$bCount = $page_items->filterBy($filter_key, $b, ',')->count();
+		return strcmp($bCount, $aCount);
+	});
+}
+
+// Sort filter values (e.g. tags) alphabetical
+if($sort == 'abc') {
+	sort($filter_values);
+}
+
 ////////////////////////////////////////////////////////// ?>
 
-<div class="filters">
-	<h2 class="is-hidden-visually">Filter blog posts by&hellip;</h2>
-	<?php /*<a class="filterbutton js-expandbutton" href="#expandthis" title="Show more filter tags">More</a> */ ?>
-	<ul class="filterlist<?php /* js-expandtarget */ ?>">
-		<?php if($filter_value): ?>
-			<li class="filterlist__item is-active">
-				<a href="<?php echo url($page->url()); ?>" class="filterlist__button" title="Remove filter: &lsquo;<?php echo tagunslug($filter_value); ?>&rsquo;">
-					<span class="is-hidden-visually">Remove filter: </span>
-					<?php echo tagunslug($filter_value); ?> &times;
-				</a>
-			</li>
-		<?php /*
-		<?php else: ?>
-			<li class="filterlist__item is-active"><span class="filterlist__button">All</span></li>
-		*/ ?>
-		<?php endif; ?>
-
-		<?php foreach($page->children()->visible()->flip()->pluck($filter_key, ',', true) as $filter_item): ?>
-			<?php /*
-			<?php if($filter_value && $filter_value == tagslug($filter_item)): ?>
-				<li class="filterlist__item is-hidden"><span class="filterlist__button"><?php echo html($filter_item); ?></span></li>
-			<?php else: ?>
-			*/ ?>
-			<?php if(!$filter_value || $filter_value != tagslug($filter_item)): ?>
-				<li class="filterlist__item"><a href="<?php echo url($page->url() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . tagslug($filter_item)) ?>" class="filterlist__button" title="Add filter: &lsquo;<?php echo html($filter_item); ?>&rsquo;"><?php echo html($filter_item); ?></a></li>
+<?php if($filter_values): ?>
+	<div class="filters">
+		<h2 class="is-hidden-visually">Filter blog posts by&hellip;</h2>
+		<ul class="filterlist">
+			<?php if($filter_value): ?>
+				<li class="filterslist__item is-active">
+					<a href="<?php echo $page->url(); ?>" class="filterlist__button" title="Remove filter: &lsquo;<?php echo tagunslug($filter_value); ?>&rsquo;">
+						<span class="is-hidden-visually">Remove filter: </span>
+						<?php echo tagunslug($filter_value); ?> &times;
+					</a>
+				</li>
 			<?php endif; ?>
-		<?php endforeach; ?>
-	</ul>
-</div>
+
+			<?php foreach($filter_values as $filter_item): ?>
+
+				<?php $page_items_count = $page_items->filterBy($filter_key, '==', tagunslug($filter_item), ',')->count(); // Associated number count of blog posts for the current filter value ?>
+
+				<?php if(!$filter_value || $filter_value != tagslug($filter_item)): ?>
+					<li class="filterslist__item">
+						<a href="<?php echo url($page->url() . '/' . (($filter_key == 'tags') ? 'tag' : $filter_key) . '/' . tagslug($filter_item)) ?>" class="filterlist__button" title="Add filter: &lsquo;<?php echo html($filter_item); ?>&rsquo;"><?php echo html($filter_item . ' [<small>' . $page_items_count . '</small>]'); ?></a>
+					</li>
+				<?php endif; ?>
+
+			<?php endforeach; ?>
+		</ul>
+	</div>
+<?php endif; ?>
