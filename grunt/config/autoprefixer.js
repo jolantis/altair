95eac7e94fbed86d234da46b193dbeaeecf94ba5
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-08-05 11:01:58 +0200

    Add another autoprefixer example.

--- grunt/config/autoprefixer.js
+++ grunt/config/autoprefixer.js
@@ -3,8 +3,9 @@ module.exports = function(grunt) {
 	grunt.config('autoprefixer', {
 		options: {
 			browsers: ['> 4%', 'last 2 versions', 'Firefox ESR'], // Default value is: > 1%, last 2 versions, Firefox ESR, Opera 12.1
+			// browsers: ['last 1 versions'], // Default value is: > 1%, last 2 versions, Firefox ESR, Opera 12.1
 			map: false, // enable/disable source maps
-			cascade: true, // ‘cascade’ indentation
+			cascade: true, // 'cascade' indentation
 		},
 		main: {
 			options: {
