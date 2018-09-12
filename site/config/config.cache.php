b100f343f85d6f104d8fdaa72808c762844ffd5c
Jonathan van Wunnik <jonathan@artlantis.nl>
2018-09-12 12:08:42 +0200

    Improve (fix?) Kirby's cache settings.

--- site/config/config.cache.php
+++ site/config/config.cache.php
@@ -9,8 +9,17 @@ Enable/disable Kirby's cache.
 */
 
 c::set('cache', false);
-c::set('cache.driver', 'file');                                                 // Valid values are: file, memcached and apc
-// c::set('cache.root', '/var/cache');                                          // The cache directory location
-c::set('cache.options', array('host' => 'localhost', 'port' => 11211));         // Change the default memcached driver host and port (the default is localhost and  11211)
-c::set('cache.options', array('prefix' => 'altair_'));                          // Prefix memcached keys (prevent collision of caches when pages are named the same, acros sites in a multisite environment)
-c::set('cache.ignore', array('sitemap', 'search'));                             // Wildcards can be used as well, e.g.: projects/*
+c::set('cache.driver', 'memcached');                                            // Valid values are: file, memcached and apc
+c::set('cache.autoupdate', true);                                               // Set to true to scan the content folder for updates and refresh the cache automatically
+
+c::set('cache.options', array(
+			'root' => '/var/cache',                                             // The cache directory location
+			'host' => 'localhost', 'port' => 11211,                             // Change the default memcached driver host and port (the default is localhost and 11211)
+			'prefix' => 'altair_'                                               // Prefix memcached keys (prevent collision of caches when pages are named the same, acros sites in a multisite environment)
+));
+
+c::set('cache.ignore', array(
+			'sitemap',                                                          // Wildcards can be used as well, e.g.: projects/*
+			'sitemap.xml',
+			'search'
+));
